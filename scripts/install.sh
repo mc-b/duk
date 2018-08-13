238cbe07f1b7177d49bd9b3c5f139390ebb34b44
mc-b <marcel.bernet@ch-open.ch>
2018-08-13 16:10:46 +0200

    Links in eigenes Repository

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,8 +3,8 @@
 #	Installationsscript dok
 
 # Jupyter Docker in Docker Umgebung
-kubectl create -f https://raw.githubusercontent.com/mc-b/devops/master/kubernetes/jupyter/jupyter-base.yaml 
-kubectl create -f https://raw.githubusercontent.com/mc-b/devops/master/kubernetes/jupyter/dind.yaml
+kubectl create -f https://raw.githubusercontent.com/mc-b/dok/master/jupyter/jupyter-base.yaml 
+kubectl create -f https://raw.githubusercontent.com/mc-b/dok/master/jupyter/dind.yaml
 
 # Jupyter Scripte etc. Allgemein verfuegbar machen
 cp -rpv data/* /data/
