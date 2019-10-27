4bb310d964a34bdaaa7ea761f1f658721aa3dec5
mc-b <marcel.bernet@ch-open.ch>
2019-10-27 17:59:33 +0100

    User anlegen nach duk-demo verschoben

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,38 +9,3 @@ kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/jupyter/dind.
 
 # Jupyter Scripte etc. Allgemein verfuegbar machen
 cp -rpv data/* /data/
-
-# User snoopy anlegen
-# User snoopy anlegen
-
-openssl genrsa -out snoopy.pem 2048
-openssl req -new -key snoopy.pem -out snoopy.csr -subj "/CN=snoopy"
-
-cat <<%EOF% | kubectl apply -f -
-apiVersion: certificates.k8s.io/v1beta1
-kind: CertificateSigningRequest
-metadata:
-  name: user-request-snoopy
-spec:
-  groups:
-  - system:authenticated
-  request: $(cat snoopy.csr | base64 | tr -d '\n')
-  usages:
-  - digital signature
-  - key encipherment
-  - client auth
-%EOF%
-
-kubectl certificate approve user-request-snoopy
-kubectl get csr user-request-snoopy -o jsonpath='{.status.certificate}' | base64 -d > snoopy.crt
-
-mkdir .kube
-kubectl --kubeconfig .kube/config-snoopy config set-cluster kubernetes --insecure-skip-tls-verify=true --server=https://$(hostname -I | cut -d ' ' -f 2):6443
-kubectl --kubeconfig .kube/config-snoopy config set-credentials snoopy --client-certificate=snoopy.crt --client-key=snoopy.pem --embed-certs=true
-
-kubectl --kubeconfig .kube/config-snoopy config set-context snoopy --cluster=kubernetes --user=snoopy 
-kubectl --kubeconfig .kube/config-snoopy config use-context snoopy
-
-kubectl apply -f rbac/
-
-kubectl describe secret $(kubectl get secret | grep snoopy | awk '{print $1}') >dashboard-snoopy.txt
