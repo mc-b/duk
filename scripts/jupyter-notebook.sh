12123830a36c291786e8fe173c46bc0397ea495a
mc-b <marcel.bernet@ch-open.ch>
2024-06-24 21:05:17 +0200

    kompose

--- scripts/jupyter-notebook.sh
+++ scripts/jupyter-notebook.sh
@@ -44,3 +44,7 @@ cd /tmp && wget -q https://github.com/tohjustin/kube-lineage/releases/download/v
     tar xzf kube-lineage_linux_amd64.tar.gz && \
     sudo mv kube-lineage /usr/local/bin/ && \
     rm kube-lineage_linux_amd64.tar.gz 
+    
+cd /tmp && curl -L https://github.com/kubernetes/kompose/releases/download/v1.34.0/kompose-linux-amd64 -o kompose && \
+    chmod +x kompose && \
+    sudo mv ./kompose /usr/local/bin/kompose    
