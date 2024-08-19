d3dc454c5ffc45aa17affecef32aba4d6f83c192
mc-b <marcel.bernet@ch-open.ch>
2024-08-19 09:18:09 +0200

    MLG Python Libraries wieder entfernt

--- scripts/jupyter-notebook.sh
+++ scripts/jupyter-notebook.sh
@@ -5,8 +5,7 @@
 sudo apt-get install -y  --no-install-recommends jupyter-notebook python3-venv uuid
 
 # Python3 Libraries
-sudo apt-get install -y python3-flask python3-setproctitle python3-requests python3-paho-mqtt \
-                        python3-matplotlib python3-numpy python3-sklearn python3-pandas python3-seaborn 
+sudo apt-get install -y python3-flask python3-setproctitle python3-requests
 
 ln -s /data/jupyter work
 
