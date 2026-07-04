91836f007cea39d95c7812cbd8bb1c706b61a0ad
mc-b <marcel.bernet@mc-b.ch>
2026-07-04 13:13:45 +0200

    Shortcuts

---
+++ urls.tf
@@ -0,0 +1,76 @@
+
+# Unterlagen
+resource "null_resource" "desktop_url_documents" {
+  provisioner "local-exec" {
+    interpreter = ["PowerShell", "-NoProfile", "-ExecutionPolicy", "Bypass", "-Command"]
+
+    command = <<EOT
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+$ShortcutPath = Join-Path $DesktopPath "Unterlagen.url"
+
+@"
+[InternetShortcut]
+URL=http://control-01-default.mshome.net:18888/lab/tree/Documents
+IconFile=C:\Windows\System32\shell32.dll
+IconIndex=220
+"@ | Set-Content -Path $ShortcutPath -Encoding ASCII
+EOT
+  }
+}
+
+# Übungen
+resource "null_resource" "desktop_url_exercises" {
+  provisioner "local-exec" {
+    interpreter = ["PowerShell", "-NoProfile", "-ExecutionPolicy", "Bypass", "-Command"]
+
+    command = <<EOT
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+$ShortcutPath = Join-Path $DesktopPath "Übungen.url"
+
+@"
+[InternetShortcut]
+URL=http://control-01-default.mshome.net:18888/lab/tree/duk
+IconFile=C:\Windows\System32\shell32.dll
+IconIndex=220
+"@ | Set-Content -Path $ShortcutPath -Encoding ASCII
+EOT
+  }
+}
+
+# K8s Dashboard
+resource "null_resource" "desktop_url_dashboard" {
+  provisioner "local-exec" {
+    interpreter = ["PowerShell", "-NoProfile", "-ExecutionPolicy", "Bypass", "-Command"]
+
+    command = <<EOT
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+$ShortcutPath = Join-Path $DesktopPath "Dashboard.url"
+
+@"
+[InternetShortcut]
+URL=https://control-01-default.mshome.net:30443
+IconFile=C:\Windows\System32\shell32.dll
+IconIndex=220
+"@ | Set-Content -Path $ShortcutPath -Encoding ASCII
+EOT
+  }
+}
+
+# Headlamp
+resource "null_resource" "desktop_url_headlamp" {
+  provisioner "local-exec" {
+    interpreter = ["PowerShell", "-NoProfile", "-ExecutionPolicy", "Bypass", "-Command"]
+
+    command = <<EOT
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+$ShortcutPath = Join-Path $DesktopPath "Headlamp.url"
+
+@"
+[InternetShortcut]
+URL=http://control-01-default.mshome.net:30444
+IconFile=C:\Windows\System32\shell32.dll
+IconIndex=220
+"@ | Set-Content -Path $ShortcutPath -Encoding ASCII
+EOT
+  }
+}
