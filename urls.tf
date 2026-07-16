a07c651abdda100dadf37233ab615eef098e4d7b
mc-b <marcel.bernet@mc-b.ch>
2026-07-16 20:12:21 +0200

    Dashboard eleminiert

--- urls.tf
+++ urls.tf
@@ -64,38 +64,6 @@ EOT
   }
 }
 
-# K8s Dashboard
-resource "null_resource" "desktop_url_dashboard" {
-  provisioner "local-exec" {
-    interpreter = ["PowerShell", "-NoProfile", "-ExecutionPolicy", "Bypass", "-Command"]
-
-    command = <<EOT
-$DesktopPath = [Environment]::GetFolderPath("Desktop")
-$ShortcutPath = Join-Path $DesktopPath "Dashboard.url"
-
-@"
-[InternetShortcut]
-URL=https://control-01-default.mshome.net:30443
-IconFile=C:\Windows\System32\shell32.dll
-IconIndex=220
-"@ | Set-Content -Path $ShortcutPath -Encoding ASCII
-EOT
-  }
-  provisioner "local-exec" {
-    when        = destroy
-    interpreter = ["PowerShell", "-NoProfile", "-ExecutionPolicy", "Bypass", "-Command"]
-
-    command = <<EOT
-$DesktopPath = [Environment]::GetFolderPath("Desktop")
-$ShortcutPath = Join-Path $DesktopPath "Dashboard.url"
-
-if (Test-Path $ShortcutPath) {
-  Remove-Item -Path $ShortcutPath -Force
-}
-EOT
-  }
-}
-
 # Headlamp
 resource "null_resource" "desktop_url_headlamp" {
   provisioner "local-exec" {
